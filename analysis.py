3b228637e20cb92ffb8083856a1b3ffa33cf2946
alexgoodell <alexgoodell@gmail.com>
2015-06-10 12:25:34 -0700

    mmmSugar python

--- analysis.py
+++ analysis.py
@@ -12,9 +12,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-mastercsv = pd.read_csv('tmp/master.csv')
 
-df = mastercsv
+os.chdir('/Applications/XAMPP/xamppfiles/htdocs/ghd_model_go/gocode/src/github.com/alexgoodell/go-mdism/tmp/')
+
+
+df = pd.read_csv('master39463.276246739.csv', header=0, skip_blank_lines=1)
+
+# mastercsv = pd.read_csv('tmp/master.csv')
+
 df = df[df.columns].astype(int)
 hivdf = df[df.Model_id == 0]
 newhivdf = hivdf.drop('Model_id', axis=1, level=None, inplace=False)
@@ -22,3 +27,9 @@ final = newhivdf.pivot_table(index='Person_id',columns='Cycle_id')
 
 for i in final.index: 
     plt.plot(final.iloc[i-1])
+    
+people_with_nafld =   [0] * 10000
+for i in range(0, 19):
+    newdfl = df[df.State_id == 3]
+    people_with_nafld[i] = len(newdfl[newdfl.Cycle_id == i])
+    
